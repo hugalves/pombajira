1d2278bfff597c09aed988809a382ab8ad2a6474
Hugo Gomes <hugo.gomes@locaweb.com.br>
2017-09-21 13:31:13 -0300

    removes useless methods

--- app/models/wrappers/jira/history.rb
+++ app/models/wrappers/jira/history.rb
@@ -1,9 +1,6 @@
 module Wrappers
   module Jira
     class History
-      INITIAL_STATUS = 'Product Backlog'
-      FINAL_STATUS   = 'Done'
-
       attr_reader :builder, :adapter, :histories
 
       def initialize(builder)
@@ -16,17 +13,19 @@ module Wrappers
         time_of(status)
       end
 
+      private
+
       def time_of(status)
         story   = fetch_by(status)
         from_id = from_id(story)
 
-        time    = with_status_items.select do |h|
-          h.items.detect do |i|
-            i['to'] == from_id
+        time    = with_status_items.select do |history|
+          history.items.detect do |item|
+            item['to'] == from_id
           end
         end.first
 
-        time.try(:created_at)
+        time.created_at
       end
 
       def fetch_by(status)
@@ -35,30 +34,6 @@ module Wrappers
             item['toString'] == status
           end
         end
-      end
-
-      def fetch_time_of_initial_status
-        histories.first['created_at']
-      end
-
-      def initial_status?(status)
-        status == INITIAL_STATUS
-      end
-
-      def fetch_time_of_final_status
-        histories.first['created_at']
-      end
-
-      def final_status?(status)
-        status == FINAL_STATUS
-      end
-
-      def to_id(story)
-        story
-          .first
-          .items
-          .detect { |h| h['field'] == 'status' }
-          .fetch('to')
       end
 
       def from_id(story)
@@ -70,7 +45,11 @@ module Wrappers
       end
 
       def with_status_items
-        histories.select do |history| history.items.any? { |item| item['field'] == 'status' } end
+        histories.select do |story|
+          story.items.any? do |item|
+            item['field'] == 'status'
+          end
+        end
       end
 
       def all
